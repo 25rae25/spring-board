de20dc47b72768dbeab98c2eb3e2240106cadfc1
25rae25 <whdudfo0124@gmail.com>
2023-01-10 22:55:47 +0900

    second commit

--- src/main/java/controller/BoardController.java
+++ src/main/java/controller/BoardController.java
@@ -2,14 +2,13 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-@RequestMapping("/board/**")
 public class BoardController {
 
-    @GetMapping("/hello")
-    public String Hello() {
-        return "/boards/hello";
+    @GetMapping("/")
+    public String board() {
+        return "board";
     }
 }
